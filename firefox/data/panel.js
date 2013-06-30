af4d2eb13c534eff4077cdb82b76f8765f9dabe9
Irina <eira@mail.ru>
2013-06-30 13:24:52 +0400

    prevalidate digit values

--- firefox/data/panel.js
+++ firefox/data/panel.js
@@ -1,22 +1,8 @@
 //@TODO replace id to js-class
 
 (function(){
-	document.getElementById("spamStart").onclick = function() {
-		console.log('spamStart button fire');
 
-		//@TODO check options before send
-		self.port.emit("spamStart", {
-			countArmy:150,
-			unitId: document.getElementById('unitId').value,
-			onlyCreate: false,
-			delay: 5,
-			seriesArmyCount: 3,
-			ring: 3,
-			compl: 123,
-			sota: 1
-		});
-	};
-
+/*
 	//открываем управляющую панель при событии
 	self.port.on("show-panel", function(){
 		console.log('show panel port on');
@@ -38,7 +24,7 @@
 		elemLog.innerHTML += d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ': ' + message + "\n";
 		elemLog.scrollTop = elemLog.scrollHeight;
 	});
-
+*/
 	//удалять кнопочки при включенной галочке
 	document.getElementById("js-only-create").onchange = function(){
 		if (this.checked){
@@ -47,4 +33,35 @@
 			document.getElementById("js-send-army-panel").classList.remove("hide");
 		}
 	}
+
+	document.getElementById("spamStart").onclick = function() {
+		console.log('spamStart button fire');
+
+		//@TODO check options before send
+		self.port.emit("spamStart", {
+			countArmy:150,
+			unitId: document.getElementById('unitId').value,
+			onlyCreate: false,
+			delay: 5,
+			seriesArmyCount: 3,
+			ring: 3,
+			compl: 123,
+			sota: 1
+		});
+	};
+
+	(function(){
+		var elems = document.getElementsByClassName("js-prevalidate");
+		for (var i in elems){
+			elems[i].onkeyup = function(){
+				var intValue = parseInt(this.value,10);
+				if (isNaN(intValue)|| intValue < this.min){
+					intValue = this.min;
+				}else if (intValue > this.max){
+					intValue=this.max;
+				}
+				this.value = intValue;
+			}
+		}
+	})();
 })();
